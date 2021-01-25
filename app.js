0cb3716d7051ef59675687b50cb745c411406a8b
Amy <min.may.flow.2@gmail.com>
2021-01-25 14:37:47 -0500

    update secret submission

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ const userSchema = new mongoose.Schema ({
   email: String,
   password: String,
   googleId: String,
-  facebookId: String
+  facebookId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -115,16 +116,45 @@ app.get("/register", function(req, res) {
 });
 
 app.get("/secrets", function(req, res) {
+  User.find({"secret": {$ne: null}}, function(err, foundUsers) {
+    if(err)
+      console.log(err);
+    else {
+      if(foundUsers) {
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
+
+app.get("/submit", function(req, res) {
   if(req.isAuthenticated())
-    res.render("secrets");
+    res.render("submit");
   else
-    res.redirect("register");
-})
+    res.redirect("login");
+});
 
 app.get("/logout", function(req, res) {
   req.logout();
   res.redirect("/");
-})
+});
+
+app.post("/submit", function(req, res) {
+  const submittedSecret = req.body.secret;
+
+  User.findById(req.user.id, function(err, foundUser) {
+    if(err)
+      console.log(err);
+    else {
+      if(foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function() {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+});
 
 app.post("/register", function(req, res) {
   User.register({username: req.body.username}, req.body.password, function(err, user) {
